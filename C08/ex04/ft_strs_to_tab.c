761515ddaa76e02d43e8aacf838c498628c50736
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2024-08-08 17:47:15 +0700

    add ex04

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -10,9 +10,76 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "ft_stock_str.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*ptr;
+	int	i;
+	int	k;
+
+	i = 0;
+	k = ft_strlen(src);
+	ptr = ((char *)malloc(k * sizeof(src) + 1));
+	if (ptr == NULL)
+		return (ptr);
+	while (src[i] != '\0')
+	{
+		ptr[i] = src[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
 
 struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
-	
+	int	i;
+	t_stock_str	*tab;
+
+	tab = malloc(sizeof(t_stock_str) * (ac + 1));
+	i = 0;
+	if (tab == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].copy = ft_strdup(av[i]);
+		tab[i].str = av[i];
+		i++;
+	}
+	tab[i].str = 0;
+	return (tab);
 }
+
+/*#include <stdio.h> 
+int    main(int argc, char *argv[])
+{
+    t_stock_str    *stock;
+
+    if (argc > 1)
+    {
+        stock = ft_strs_to_tab(argc - 1, ++argv);
+        while (stock->str)
+        {
+            printf("str: %s\n", stock->str);
+            printf("copy: %s\n", stock->copy);
+            printf("size: %d\n", stock->size);
+            stock++;
+            printf("\n");
+        }
+    }
+    printf("\n");
+    return (0);
+}*/
